55d58928928310c10ca81afa6bf688240702e436
Pushpak_Doodleblue <pushpak@doodleblue.com>
2020-12-17 14:15:33 +0530

    RoomDB(C.U.R.D.)

---
+++ app/src/main/java/com/roomdbb/roomdbb/fragments/list/ListAdapter.kt
@@ -0,0 +1,36 @@
+package com.roomdbb.roomdbb.fragments.list
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.roomdbb.roomdbb.R
+import com.roomdbb.roomdbb.data.User
+import kotlinx.android.synthetic.main.custom_row.view.*
+
+class ListAdapter : RecyclerView.Adapter<ListAdapter.MyViewHolder>() {
+    private var userList = emptyList<User>()
+    class MyViewHolder(item:View):RecyclerView.ViewHolder(item){
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        return MyViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.custom_row,parent,false))
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        val currentItem= userList[position]
+        holder.itemView.tvId.text= currentItem.id.toString()
+        holder.itemView.tvFname.text=currentItem.firstName
+        holder.itemView.tvLName.text= currentItem.lastName
+        holder.itemView.tvAge.text= currentItem.age.toString()
+    }
+
+    override fun getItemCount(): Int {
+      return  userList.size
+    }
+    fun setData(user:List<User>){
+        this.userList=user
+        notifyDataSetChanged()
+    }
+}
--- app/src/main/java/com/roomdbb/roomdbb/fragments/list/ListFragment.kt
+++ app/src/main/java/com/roomdbb/roomdbb/fragments/list/ListFragment.kt
@@ -5,12 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.roomdbb.roomdbb.R
 import com.roomdbb.roomdbb.data.UserViewModel
 import kotlinx.android.synthetic.main.fragment_list.view.*
-
 
 class ListFragment : Fragment() {
     lateinit var userViewModel: UserViewModel
@@ -24,6 +24,11 @@ class ListFragment : Fragment() {
         view.floatingActionButton.setOnClickListener {
             findNavController().navigate(R.id.action_listFragment_to_addFragment)
         }
+        val adapter= ListAdapter()
+        view.rvUserData.adapter=adapter
+        userViewModel.readAllData.observe(viewLifecycleOwner, Observer { user ->
+            adapter.setData(user)
+        })
         return view
     }
 }
